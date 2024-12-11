c19f0ff92a532c127c9847ebb7b19aab124c19dc
Kobe Swier <kobe.swier@southeasttech.edu>
2024-12-11 03:45:06 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using CombatGame.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CombatGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
