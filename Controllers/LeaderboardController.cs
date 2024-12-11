7b271abf65717ea9e0bf7858ea983d11314399f3
Kobe Swier <kobe.swier@southeasttech.edu>
2024-12-11 04:21:54 -0600

    12/10

---
+++ Controllers/LeaderboardController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CombatGameWebApp.Models;
+using CombatGame.Data;
+using System.Linq;
+
+namespace CombatGame.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LeaderboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var leaderboard = _context.Battles
+                .GroupBy(b => b.Winner)
+                .Select(g => new { TeamName = g.Key, Wins = g.Count() })
+                .OrderByDescending(g => g.Wins)
+                .ToList();
+
+            return View(leaderboard);
+        }
+    }
+}
